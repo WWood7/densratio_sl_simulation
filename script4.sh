c17df2ab1376f5b92a9007e7123127678635053d
Wencheng Wu <menacewood7@gmail.com>
2023-11-13 10:04:50 -0500

    add visualize part

--- script4.sh
+++
@@ -1,9 +0,0 @@
-#SBATCH --array=1-10
-#SBATCH --partition=day-long-cpu
-#SBATCH --error=/home/wwu227/thesis/oracle/scratch/err/oracle4.err
-#SBATCH --output=/home/wwu227/thesis/oracle/scratch/out/oracle4.out
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate rproj
-Rscript sim_oracle.R 4
-#SBATCH --mail-user=wwu227@emory.edu
-#SBATCH --mail-type=ALL
