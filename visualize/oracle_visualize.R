7565cc5ce57457833f33f532765453a0310f72ed
Wencheng Wu <menacewood7@gmail.com>
2023-12-11 12:49:15 -0500

    Update oracle_visualize.R

--- visualize/oracle_visualize.R
+++ visualize/oracle_visualize.R
@@ -24,9 +24,15 @@ for (i in 1:4){
 # draw the plots
 for (i in 1:4){
     data <- results[[i]][, c(1, 3)]
+    data <- data[order(data$learner), ]
+    data$learner <- c(rep('lr7', 100), rep('lr2', 100), rep('lr3', 100), rep('lr1', 100), 
+                      rep('lr4', 100), rep('lr5', 100), rep('lr8', 100), rep('lr6', 100),
+                      rep('sl', 100))
     # Calculate the mean for each learner
     mean_data <- aggregate(WNLL ~ learner, data = data, FUN = mean)
-    
+    mean_data <- mean_data[order(mean_data$learner), ]
+    mean_data$learner <- c('lr1', 'lr2', 'lr3', 'lr4', 'lr5', 'lr6', 'lr7', 'lr8', 'sl')
+
     # Rename the mean column for clarity
     names(mean_data)[which(names(mean_data) == "WNLL")] <- "mean"
     
