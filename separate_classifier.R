80d07f77101dda632cb2c46af9a73372f6bf12af
Wencheng Wu <menacewood7@gmail.com>
2024-01-19 10:59:18 -0500

    Update separate_classifier.R

--- separate_classifier.R
+++ separate_classifier.R
@@ -23,44 +23,41 @@ setdata <- function(n){
 
 # define classification super learners
 cl1 <- Lrnr_bayesglm$new()
-cl2 <- Lrnr_dbarts$new()
-cl3 <- Lrnr_glm$new()
-cl4 <- Lrnr_hal9001$new()
+cl2 <- Lrnr_glm$new()
+cl3 <- Lrnr_hal9001$new()
 
 # define learners
-lr1 <- Pipeline$new(Lrnr_densratio_kernel$new(method = 'RuLSIF', kernel_num = 100, alpha = 0.5, name = 'lr1'), 
-                    Lrnr_densratio_kernel$new(method = 'RuLSIF', kernel_num = 100, alpha = 0.5, name = '', stage2 = TRUE))
-lr2 <- Pipeline$new(Lrnr_densratio_kernel$new(method = 'RuLSIF', kernel_num = 100, alpha = 0.8, name = 'lr2'), 
-                    Lrnr_densratio_kernel$new(method = 'RuLSIF', kernel_num = 100, alpha = 0.8, name = '', stage2 = TRUE))
-lr3 <- Pipeline$new(Lrnr_densratio_kernel$new(method = 'RuLSIF', kernel_num = 100, alpha = 0.8, name = 'lr2'), 
-                    Lrnr_densratio_kernel$new(method = 'RuLSIF', kernel_num = 100, alpha = 0.8, name = '', stage2 = TRUE))
+lr1 <- Pipeline$new(Lrnr_densratio_classification$new(name = 'lr6'), 
+                    Lrnr_densratio_classification$new(stage2 = TRUE, name = ''))
+lr2 <- Pipeline$new(Lrnr_densratio_classification$new(classifier = make_learner(Lrnr_bayesglm), name = 'lr7'), 
+                    Lrnr_densratio_classification$new(classifier = make_learner(Lrnr_bayesglm), stage2 = TRUE, name = ''))
+lr3 <- Pipeline$new(Lrnr_densratio_classification$new(classifier = make_learner(Lrnr_hal9001), name = 'lr7'), 
+                    Lrnr_densratio_classification$new(classifier = make_learner(Lrnr_hal9001), stage2 = TRUE, name = ''))
 lr4 <- Pipeline$new(Lrnr_densratio_kernel$new(method = 'KLIEP', kernel_num = 100, fold_num = 4, name = 'lr4'), 
                     Lrnr_densratio_kernel$new(method = 'KLIEP', kernel_num = 100, fold_num = 4, name = '', stage2 = TRUE))
-lr5 <- Lrnr_densratio_kernel$new(method = 'KLIEP', kernel_num = 200, 
-                                 fold_num = 8, name = 'lr5')
-lr6 <- Pipeline$new(Lrnr_densratio_classification$new(name = 'lr6'), 
-                    Lrnr_densratio_classification$new(stage2 = TRUE, name = ''))
 
 # stack the learners into a super learner
-stack_cl <- Stack$new(cl1, cl2, cl3, cl4)
+stack_cl <- Stack$new(cl1, cl2, cl3)
 csl <- Lrnr_sl$new(stack_cl, metalearner = Lrnr_solnp$new(
     eval_function = loss_loglik_binomial))
 
 
 # stack the learners into a super learner
-stack <- Stack$new(lr1, lr2, lr3, lr4, lr5, lr6) 
+stack <- Stack$new(lr1, lr2, lr3) 
 sl <- Lrnr_sl$new(stack, metalearner = Lrnr_solnp$new(
     eval_function = loss_weighted_loglik_densratio ))
 
 
-n = 1000
+n = 300
 
 # generate data
 data <- setdata(n)
 data$indicator <- data$a
 # normalize the data
-data$m <- data$m / sd(data$m)
-data$x <- data$x / sd(data$x)
+sd_m <- sd(data$m)
+sd_x <- sd(data$x)
+data$m <- data$m / sd_m
+data$x <- data$x / sd_x
 # define a task
 task1 <- sl3_Task$new(data = data, covariates = c('m', 'x', 'w'), outcome = 'indicator', folds = 3)
 # pass the folds argument from task1 so the two tasks have the same cv folds
@@ -72,9 +69,10 @@ sl_fit <- sl$train(task1)
 
 
 # test set
-n = 200
+n = 400
 data <- setdata(n)
-
+data$m <- data$m / sd_m
+data$x <- data$x / sd_x
 # 从这里开始
 pre_task <- sl3_Task$new(data = data, covariates = c('m', 'x', 'w'))
 pre_task2 <- sl3_Task$new(data = data, covariates = c('x', 'w'))
@@ -83,10 +81,12 @@ pre_task2 <- sl3_Task$new(data = data, covariates = c('x', 'w'))
 cl_pre1 <- csl1_fit$predict(task = pre_task)
 cl_pre2 <- csl2_fit$predict(task = pre_task2)
 csl_results <- (cl_pre1 / (1 - cl_pre1)) / (cl_pre2 / (1 - cl_pre2))
-
 sl_results <- sl_fit$predict(task = pre_task)
-
-
-
+ 
+# calculate the risks
+csl_risk <- -log(bound(csl_results, c(0.01, 10))) * (data$a - 0.5) * 2
+sl_risk <- -log(bound(sl_results, c(0.01, 10))) * (data$a - 0.5) * 2
+csl_risk <- mean(csl_risk)
+sl_risk <- mean(sl_risk)
 
 
