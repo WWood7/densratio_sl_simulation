1c3675d1ea9a7462703c7a6d0c69d4c5ce265212
Wencheng Wu <menacewood7@gmail.com>
2024-02-18 23:41:49 -0500

    Update mediation1_diagnostic.sh

--- simulation_mediation1/mediation1_diagnostic.sh
+++ simulation_mediation1/mediation1_diagnostic.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=/home/wwu227/thesis/mediation1_diagnostic/output_%j.txt  # Standard output; %j is replaced with the job ID
 #SBATCH --error=/home/wwu227/thesis/mediation1_diagnostic/error_%j.txt    # Standard error; %j is replaced with the job ID
 #SBATCH --ntasks=1
-
+#SBATCH --partition=benkeser
 # Activate conda environment
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate rproj
